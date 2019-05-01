da38247f24c74b1f66bbfdc55903effb980751f9
jannymac <jannymac@users.noreply.github.com>
2019-05-01 11:05:33 -0400

    Update functions.php
    Inserted function to get the category id, so that I can output it in the TWIG file which renders the calendar view.
    I'll use the class generated to style via CSS.

--- listly/functions.php
+++ listly/functions.php
@@ -8,5 +8,9 @@
  * This file is optional and can be omitted from your custom theme.
  */
 
+function category_id() {
+	$my_category = get_term_by( 'id', $post_categories[0]->cat_ID, 'ai1ec_event' );
+	echo "$my_category";
+}
 
 ?>
