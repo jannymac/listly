d52e32b6793031ac849212cb3a4ad4aacf85648f
jannymac <jannymac@users.noreply.github.com>
2019-05-01 10:45:39 -0400

    Add files via upload
    This is the original (TWIG) theme I created for the All-in-One Event Calendar by Timely for WordPress.

---
+++ listly/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * For advanced users:
+ *
+ * Place any custom WordPress hooks or theme functions into this file.
+ *
+ * This file is optional and can be omitted from your custom theme.
+ */
+
+
+?>
